b3b67416e4f2ef66d241a18d657efd39ecc1c619
suriya-it19 <suriya.it19@bitsathy.ac.in>
2022-08-01 16:32:39 +0530

    added final code

--- app/src/main/java/com/example/todoapp/MainActivity.kt
+++ app/src/main/java/com/example/todoapp/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.todoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var todoAdapter: TodoAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        todoAdapter = TodoAdapter(ArrayList())
+
+        rvTodo.adapter =todoAdapter
+        rvTodo.layoutManager = LinearLayoutManager(this)
+
+        btAddTodo.setOnClickListener() {
+            val todoTitle = etTodo.text.toString()
+            if(todoTitle.isEmpty()) {
+                val todo = Todo(todoTitle)
+                todoAdapter.addTodo(todo)
+                etTodo.text.clear()
+            }
+        }
+        btRemoveTodo.setOnClickListener() {
+            todoAdapter.deleteDoneTodos()
+        }
     }
 }
--- app/src/main/java/com/example/todoapp/TodoAdapter.kt
+++ app/src/main/java/com/example/todoapp/TodoAdapter.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.item_todo.view.*
 
 // to hold the list to store all the todos
 // to have a view holder
