9cca21238eee3cb0ea2abf2538c1433ec2237460
suriya-it19 <suriya.it19@bitsathy.ac.in>
2022-08-02 10:52:58 +0530

    added primary constructor

--- app/src/main/java/com/example/todoapp/Todo.kt
+++ app/src/main/java/com/example/todoapp/Todo.kt
@@ -3,7 +3,7 @@ package com.example.todoapp
 // data class like structs/user defined data type in C
 // to mold a set of data as a single data, for associated
 // data processing
-data class Todo {
-    val title: String = "Todo App"
+data class Todo(
+    var title: String = "Todo App",
     var isChecked: Boolean = false
-}
+)
